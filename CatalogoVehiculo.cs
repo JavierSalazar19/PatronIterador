b40e6e6eeba58f171869445942799fa46ec172ea
JavierSalazar19 <javierasc2002@gmail.com>
2023-12-05 09:53:32 -0700

    Agregar archivos de proyecto.

---
+++ CatalogoVehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronIterador
+{
+    public class CatalogoVehiculo: Catalogo<Vehiculo,IteradorVehiculo>
+    {
+        public CatalogoVehiculo() 
+        {
+            contenido.Add(new Vehiculo("vehiculo economico"));
+            contenido.Add(new Vehiculo("vehiculo pequeño economico"));
+            contenido.Add(new Vehiculo("vehiculo premium"));
+            contenido.Add(new Vehiculo("vehiculo troca de Jazmin"));
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronIterador
+{
+    public class Vehiculo : Elemento
+    {
+        public Vehiculo(string descripcion) : base(descripcion) { }
+        public void visualiza()
+        {
+            Console.WriteLine("Descripcion del vehiculo: " + descripcion); 
+        }
+    }
+}
